ffdecdad3d0999794c7d5c8dd080cea39bacd96a
Giritharan Balasubramaniam <giri@Giritharans-MacBook-Pro.local>
2021-12-20 10:02:15 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template : `<div>{{title}}<h1>Product management</h1></div>`
+
 })
 export class AppComponent {
   title = 'apm-new';
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'pm-products',
+    templateUrl : './product-list.html'
+})
+
+
+export class ProductListComponent {
+    
+
+
+
+}
