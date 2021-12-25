993ebcef0f8b23f913e9d2541ef30af7c388705a
Giritharan Balasubramaniam <giri@Giritharans-MacBook-Pro.local>
2021-12-24 23:04:34 -0600

    enhancements for product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template : `<div>{{title}}<h1>Product management</h1></div>`
+  template : `<div>{{title}}<h1>Product management</h1>
+              <pm-products></pm-products>
+              </div>`
 
 })
 export class AppComponent {
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,7 +8,36 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent {
     
+    pageTitle :String = 'Product List';
+    showImage : boolean = false;
+    listFilter : String = 'cart';
+    products : any[] = [
+      {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2021",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/leaf_rake.png"
+      },
+      {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2021",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "assets/images/garden_cart.png"
+      }
+    ];
 
 
+    toggleImage () : void {
+      console.log(this.showImage)
+      this.showImage = !this.showImage;
+    }
 
 }
